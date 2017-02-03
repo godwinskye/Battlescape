5bd237e3a207e19ec6c5c5f8e548a4159072dc50
Godwin Stephens <godwin.skye@gmail.com>
2017-02-02 19:10:25 -0800

    Base Project

---
+++ Source/Battlescape/Battlescape.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Battlescape.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Battlescape, "Battlescape" );
